e4aa9905dbac0f262181b30ae6b3d47af06ff729
Bryce Simonds <bsimonds14@gmail.com>
2022-05-20 15:36:43 -0600

    feature: valid_placement almost done

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Board do
 
 
 
-  it "is coordinate on board" do
+  it "is NOT coordinate on board" do
     board = Board.new
   expect(board.valid_coordinate?("A5")).to eq(false)
   expect(board.valid_coordinate?("A22")).to eq(false)
@@ -37,13 +37,37 @@ RSpec.describe Board do
   expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
   end
 
-  # it "ship coordinates are NOT consecutive" do
-  #   board = Board.new
-  #   cruiser = Ship.new("Cruiser", 3)
-  #   submarine = Ship.new("Submarine", 2)
-  # expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
-  # expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
-  # expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
-  # expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
-  # end
+  it "ship is NOT the same length as number of coordinates in array " do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+  expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+  expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it "ship coordinates are NOT consecutive" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+  expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+  expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+  expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+  expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+  end
+
+  it "ship coordinates are consecutive" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+  expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
+  expect(board.valid_placement?(submarine, ["A1", "B1"])).to eq(true)
+  expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+  expect(board.valid_placement?(submarine, ["C1", "C2"])).to eq(true)
+  end
+
+
+
+
+
+
 end
