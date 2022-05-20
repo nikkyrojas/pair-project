c685021ccc5645d69f65a7f69cd4170d2d85cb8f
Bryce Simonds <bsimonds14@gmail.com>
2022-05-19 18:35:13 -0600

    test: coordinats in array same as length of ship

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -28,4 +28,12 @@ RSpec.describe Board do
   expect(board.valid_coordinate?("A5")).to eq(false)
   expect(board.valid_coordinate?("A22")).to eq(false)
   end
+
+  it "ship placement is valid" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+  expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+  expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
 end
