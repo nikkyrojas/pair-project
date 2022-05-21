ace7465acdc324c3207556e809b3f8f9f3f2d182
Bryce Simonds <bsimonds14@gmail.com>
2022-05-21 10:06:45 -0600

    feature: valid placement checks for overlapping ships

--- lib/board.rb
+++ lib/board.rb
@@ -35,28 +35,37 @@ class Board
     end
   end
 
-
   def valid_placement? (ship, array_coordinates)
-    if ship.length == array_coordinates.count #&& @render_status = "."
-      the_keys = @cells.keys
-      horizontal_validation = the_keys.each_cons(ship.length).to_a #["A1", "A2"..]..["D2", "D3", "D4"]
-      flip_keys = the_keys.map do |key|
-         reverse_element(key) #["1A, "2A", "3A"..]
-       end
-       flip_keys_sorted = flip_keys.sort #["1A, "1B", "1C"..]
-       letter_first = flip_keys_sorted.map do |key| #this creates => ["A1", "B1", "C1"..]
-         reverse_element(key)
-       end
-       vertical_validation = letter_first.each_cons(ship.length).to_a
-       if horizontal_validation.include?(array_coordinates) || vertical_validation.include?(array_coordinates)
-         true
-       else
-         false
-       end
-    elsif ship.length != array_coordinates.count || place.includes?(ship)
+    if ship.length != array_coordinates.count
       false
-    end
-  end
-end
+    elsif ship.length == array_coordinates.count
+      is_cell_empty = array_coordinates.map do |array_coordinate|
+      @cells[array_coordinate].empty?
+      end
+
+        if is_cell_empty.any?(false)
+          false
+
+        elsif
+          the_keys = @cells.keys
+          horizontal_validation = the_keys.each_cons(ship.length).to_a #["A1", "A2"..]..["D2", "D3", "D4"]
+          flip_keys = the_keys.map do |key|
+             reverse_element(key) #["1A, "2A", "3A"..]
+           end
+           flip_keys_sorted = flip_keys.sort #["1A, "1B", "1C"..]
+           letter_first = flip_keys_sorted.map do |key| #this creates => ["A1", "B1", "C1"..]
+             reverse_element(key)
+           end
+           vertical_validation = letter_first.each_cons(ship.length).to_a
+
+           if horizontal_validation.include?(array_coordinates) || vertical_validation.include?(array_coordinates)
+             true
+           else
+             false
+           end
+         end
+       end
+     end
+   end
 
 #if @cells has ship return false or if empty = true
