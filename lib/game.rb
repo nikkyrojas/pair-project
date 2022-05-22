dfc56974f68be9bc580fb1968f26e806e901bed8
Nikky Rojas <rojas7313@gmail.com>
2022-05-22 15:17:33 -0600

    start board class

--- lib/game.rb
+++ lib/game.rb
@@ -4,18 +4,36 @@ class Game
   end
 
   def start_game
+
     puts "Start Game!"
     puts "Welcome to Battleship!"
     puts "Enter p to play. Enter q to quit."
+
     start_game = gets
-    if start_game == p
+    if start_game.chomp == "p" or "P"
+      computer_board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
 
-    elsif start_game == q
 
+      shuffle_coordinates = @cells.keys.sample(3)
+        until
+
+        end
+
+      if computer_board.valid_placement?(cruiser, shuffle_coordinates) == true
+
+      computer_board.place(cruiser, @cells.keys.sample(3))
+      computer_board.render
+
+
+      player_board = Board.new
+
+
+      until start_game.chomp == "q" or "Q"
+      puts "Bye!"
     else
       puts "error"
     end
 
   end
-start_game
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Game do
     game = Game.new
   expect(game).to be_a(Game)
   end
+
+  it "what are the random generated coordinates for computer" do
+    computer_board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    expect().to eq(shuffle_coordinates)
+
+  end
+
+  it "is the computer placement valid" do
+    computer_board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+  end
 end
