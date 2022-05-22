92b2e2968035f9c62cff13f803c76701b5b9fb70
Bryce Simonds <bsimonds14@gmail.com>
2022-05-21 18:56:43 -0600

    feature: renders a board before turns are taken

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -110,11 +110,12 @@ RSpec.describe Board do
 
   end
 
-  xit "renders a board" do
+  it "renders a board" do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
       board.place(cruiser, ["A1", "A2", "A3"])
       expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+      expect(board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
     end
 
 
