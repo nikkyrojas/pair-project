16bd03f750587ec0df953847e64536dc84dcfb39
Nikky Rojas <rojas7313@gmail.com>
2022-05-22 14:00:39 -0600

    add start_game method

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,21 @@
 class Game
   attr_reader
-def initialize
+  def initialize
+  end
+
+  def start_game
+    puts "Start Game!"
+    puts "Welcome to Battleship!"
+    puts "Enter p to play. Enter q to quit."
+    start_game = gets
+    if start_game == p
+
+    elsif start_game == q
+
+    else
+      puts "error"
+    end
+
+  end
+start_game
 end
