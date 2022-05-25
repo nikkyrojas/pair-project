4b998538c9b4c0842a7439de31a3514ca552ae46
Bryce Simonds <bsimonds14@gmail.com>
2022-05-25 17:13:46 -0600

    debug: debugged the computer board

--- lib/board.rb
+++ lib/board.rb
@@ -89,36 +89,18 @@ class Board
       false
     elsif ship.length == array_coordinates.count && @valid_coord_array.include?(array_coordinates) == true
       is_cell_empty = array_coordinates.map do |array_coordinate|
-      @cells[array_coordinate].empty?
+                      @cells[array_coordinate].empty?
       end
 
         if is_cell_empty.any?(false)
           false
+        else
+          true
+        end 
+    end
+  end
 
-        elsif
-          the_keys = @cells.keys
-          horizontal_validation = the_keys.each_cons(ship.length).to_a #["A1", "A2"..]..["D2", "D3", "D4"]
-          flip_keys = the_keys.map do |key|
-             reverse_element(key) #["1A, "2A", "3A"..]
 
-           end
-           flip_keys_sorted = flip_keys.sort #["1A, "1B", "1C"..]
-           letter_first = flip_keys_sorted.map do |key| #this creates => ["A1", "B1", "C1"..]
-             reverse_element(key)
-           end
-           vertical_validation = letter_first.each_cons(ship.length).to_a
-           combined_arrays= horizontal_validation + vertical_validation
-
-           invalid_arrays = [["A3", "A4", "B1"],["A4", "B1", "B2"],["B3", "B4", "C1"],["B4", "C1", "C2"],["C3", "C4", "D1"],["C4", "D1", "D2"],["C1", "D1", "A2"],["C2", "D2", "A3"],["C3", "D3", "A4"],["A4", "B1"],["B4", "C1"],["C4","D1"],["D1","A2"], ["D2","A3"],["D3", "A4"]]
-           final_valid_arrays = combined_arrays - invalid_arrays
-           if @valid_coord_array.include?(array_coordinates)
-             true
-           else
-             false
-           end
-         end
-       end
-     end
 
      def create_the_board(show_the_ship = false)
          if show_the_ship == false
